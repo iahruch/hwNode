a39a5fff1b3a836225ffcb5cf1040c129af7f49f
Ihor <ihruch.test@gmail.com>
2022-04-18 13:32:43 +0300

    finish first task

--- NodeEvent/hw1.js
+++ NodeEvent/hw1.js
@@ -1,14 +1,70 @@
 
+import { randomUUID } from 'crypto'
+import EventEmitter from 'events';
 
-
-class Bank {
+class Bank extends EventEmitter {
     constructor() {
-
+        super();
         this.personsData = new Map();
+        this.initListeners();
     }
 
-    async register(person) {
-        const { randomUUID } = await import('crypto');
+    initListeners() {
+        this.on("add", this.replenishClienAccount);
+        this.on('error', this.handleError);
+        this.on('get', this.getClientBalance);
+        this.on('withdraw', this.withdraw)
+    }
+
+    replenishClienAccount(clientId, amount) {
+        if (!clientId) {
+            this.emit('error', TypeError('Replenishment failed. Client Id is empty'))
+        }
+
+        if (amount <= 0) {
+            this.emit('error', TypeError('Amount must be more than 0'))
+        }
+
+        const clientData = this._getClientData(clientId);
+        clientData['balance'] += amount;
+
+    }
+
+    getClientBalance(clientId, func) {
+        if (!clientId) {
+            this.emit('error', TypeError('Replenishment failed. Client Id is empty'));
+            return;
+        }
+        
+        const clientData = this._getClientData(clientId);
+        if(!clientData) {
+            this.emit('error', TypeError('Client not found'));
+            return;
+        }
+        func(clientData['balance']);
+    }
+
+    withdraw(clientId, sum) {
+        if (!clientId) {
+            this.emit('error', TypeError('Replenishment failed. Client Id is empty'));
+            return;
+        }
+
+        const clientData = this._getClientData(clientId);
+        if(!clientData) {
+            this.emit('error', TypeError('Client not found'));
+            return;
+        }
+        
+        if( clientData['balance'] < sum || sum <=0) {
+            this.emit('error', TypeError('Not enough money to withdraw or the sum cannot be equal zero or less'));
+            return;
+        }
+        clientData['balance'] -=sum;
+
+    }
+
+    register(person) {
         let isPerson = this._checkPeson(person);
 
         if (isPerson) {
@@ -16,55 +72,54 @@ class Bank {
             return;
         }
 
-        this.personsData.set(randomUUID(), new Map(Object.entries(person)));
-        this._showData();
+        let personalId = randomUUID();
+        this.personsData.set(personalId, person);
+        //this._showData();
+        return personalId;
     }
 
+    handleError(error) {
+        console.log('Error :>> ', error.message);
+    }
 
     _checkPeson(person) {
         let response = false;
         // for (let obj of this.personsData.values()) {
-        //     console.log('1', obj);
         //     if (obj.get("name") === person.name) {
         //         return true;
         //     }
         // }
         this.personsData.forEach((valueObj, key, map) => {
-            if (valueObj.get("name") === person.name) {
+            if (valueObj["name"] === person.name) {
                 response = true;
-             }
+            }
         });
         return response;
     }
 
-    _showData() {
-        console.log('perosonsData :>> ', this.personsData);
+    _getClientData(id) {
+        const clientData = this.personsData.get(id);
+        return clientData;
     }
-
 
 }
 
 const bank = new Bank();
 
-
-
 const personId = bank.register({
     name: 'Pitter',
     balance: 100
 });
-const personId1 = bank.register({
-    name: 'Pitter J',
-    balance: 101
+//console.log('personId :>> ', personId);
+
+bank.emit('add', personId, 50);
+
+bank.emit('get', personId, (balance) => {
+    console.log(`I have ${balance}₴`); // I have 120₴
 });
 
-// bank.emit('add', personId, 20);
+bank.emit('withdraw', personId, 10);
 
-// bank.emit('get', personId, (balance) => {
-//     console.log(`I have ${balance}₴`); // I have 120₴
-// });
-
-// bank.emit('withdraw', personId, 50);
-
-// bank.emit('get', personId, (balance) => {
-//     console.log(`I have ${balance}₴`); // I have 70₴
-// })
+bank.emit('get', personId, (balance) => {
+    console.log(`I have ${balance}₴`); // I have 120₴
+});
