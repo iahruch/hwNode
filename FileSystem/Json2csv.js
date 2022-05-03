35fe74190de75bed1e0a4a537d57b40ddb65fa40
Ihor <ihruch.test@gmail.com>
2022-05-03 21:30:52 +0300

    task3 done

--- FileSystem/Json2csv.js
+++ FileSystem/Json2csv.js
@@ -1,8 +1,82 @@
 import path from 'path';
+import EventEmitter from 'stream';
+import { access, readFile, writeFile } from 'fs/promises';
+import { constants } from 'fs';
+import { EOL } from 'os';
+
+export class Json2csv extends EventEmitter {
+    constructor(filePath, fields = [], opts = {}) {
+        super(opts);
+        this.filePath = filePath;
+        this.sortFilds = new Set(fields);
+        this.initListeners();
+    }
+
+    initListeners() {
+        this.on('error', (error) => {
+            console.error("Error >> ", error);
+        })
+    }
+
+    async createCsv() {
+        console.log('object :>> ', this.filePath);
+        console.log('this.sortFilds :>> ', this.sortFilds);
+
+        try {
+            await access(this.filePath, constants.F_OK | constants.R_OK);
+            console.log('Can access');
+        } catch {
+            throw Error('File does not exist or file cannot be read');
+        }
 
 
-export class Json2csv {
-    constructor() {}
+        let isHeaders = true;
+        let delimiter = ';';
+        let csvStr = '';
+
+        try {
+            const fileData = await readFile(this.filePath, { encoding: 'utf-8' });
+            const data = JSON.parse(fileData);
+
+            data.forEach(obj => {
+                let tempStr = '';
+
+                //add headers
+                if (isHeaders) {
+                    Object.keys(obj).forEach(key => {
+                        if (this.sortFilds.has(key)) {
+                            csvStr = csvStr + key + delimiter;
+                        }
+                    })
+                    csvStr = csvStr.slice(0, csvStr.length - 1) + EOL;
+                    isHeaders = false;
+                }
+                // add data string    
+                Object.entries(obj).forEach(([key, val]) => {
+                    if (this.sortFilds.has(key)) {
+                        tempStr = tempStr + JSON.stringify(val) + delimiter;
+                    }
+                })
+
+                csvStr = csvStr + tempStr.slice(0, tempStr.length - 1) + EOL;
+            })
+
+        } catch (error) {
+            throw Error('File has not been read');
+        }
 
 
+        try {
+            await writeFile(path.resolve('comments.csv'), csvStr);
+            console.log('File has been written :>> ');
+        } catch (error) {
+            throw Error('File has not been written :>> ');
+        }
+
+
+
+    }
 }
+const fields = ['postId', 'id', 'name'];
+const json2csv = new Json2csv(path.resolve('comments.json'), fields);
+json2csv.createCsv();
--- FileSystem/index.js
+++ FileSystem/index.js
@@ -1,81 +1,71 @@
 import path from 'path';
-import EventEmitter from 'stream';
-import { access, readFile, writeFile } from 'fs/promises';
-import { constants } from 'fs';
-import { EOL } from 'os';
+import EventEmitter, { pipeline } from 'stream';
+import { access } from 'fs/promises';
+import { F_OK, R_OK, createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
 
-export class Json2csv extends EventEmitter {
-    constructor(filePath, fields = [], opts = {}) {
+
+export class Archiver extends EventEmitter {
+    constructor(opts = {}) {
         super(opts);
-        this.filePath = filePath;
-        this.sortFilds = new Set(fields);
-        this.initListeners();
+
     }
 
     initListeners() {
         this.on('error', (error) => {
             console.error("Error >> ", error);
+        });
+        this.on('close', () => {
+            console.log('Stream is closed :>> ');
         })
     }
 
-    async create() {
-        console.log('object :>> ', this.filePath);
-        console.log('this.sortFilds :>> ', this.sortFilds);
+    async create(filePath) {
+        console.log('Method create :>> ');
         try {
-            await access(this.filePath, constants.F_OK | constants.R_OK);
-            console.log('Can access');
-        } catch {
-            throw Error('File does not exist or file cannot be read');
-        }
-
-
-        let isHeaders = true;
-        let delimiter = ';';
-        let csvStr = '';
-
-        try {
-            const fileData = await readFile(this.filePath, { encoding: 'utf-8' });
-            const data = JSON.parse(fileData);
-
-            data.forEach(obj => {
-                let tempStr = '';
-
-                //add headers
-                if (isHeaders) {
-                    Object.keys(obj).forEach(key => {
-                        if (this.sortFilds.has(key)) {
-                            csvStr = csvStr + key + delimiter;
-                        }
-                    })
-                    csvStr = csvStr.slice(0, csvStr.length - 1) + EOL;
-                    isHeaders = false;
-                }
-                // add data string    
-                Object.entries(obj).forEach(([key, val]) => {
-                    if (this.sortFilds.has(key)) {
-                        tempStr = tempStr + JSON.stringify(val) + delimiter;
-                    }
-                })
-
-                csvStr = csvStr + tempStr.slice(0, tempStr.length - 1) + EOL;
-            })
-
+            await this._isFileExict(filePath);
         } catch (error) {
-            throw Error('File has not been read');
+            throw error;
         }
 
-
-        try {
-            await writeFile(path.resolve('comments.csv'), csvStr);
-            console.log('Fale has been written :>> ');
-        } catch (error) {
-            throw Error('File has not been written');
-        }
-
-
-
+        pipeline(
+            createReadStream(filePath),
+            zlib.createGzip(),
+            createWriteStream(filePath + '.zip'),
+            (err) => {
+                console.log('Error pipeline :>> ', err);
+            }
+        )
     }
+
+    async unpack(filePath) {
+        console.log('Method unpack :>> ');
+        try {
+            await this._isFileExict(filePath);
+        } catch (error) {
+            throw error;
+        }
+
+        pipeline(
+            createReadStream(filePath),
+            zlib.createGunzip(),
+            createWriteStream(path.resolve('commentsCopy.csv')),
+            (err) => {
+                console.log('Error pipeline :>> ', err);
+            }
+        )
+    }
+
+    async _isFileExict(filePath) {
+        try {
+            await access(filePath, F_OK | R_OK);
+            return true;
+        } catch (error) {
+            throw Error("File does't exist");
+        }
+    }
+
 }
-const fields = ['postId', 'id', 'name'];
-const json2csv = new Json2csv(path.resolve('comments.json'), fields);
-json2csv.create();
+const archiver = new Archiver();
+// archiver.create(path.resolve('comments.csv'));
+archiver.unpack(path.resolve('comments.csv.zip'));
